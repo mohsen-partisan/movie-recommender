a38fa8ad44fb69b14030ac0b0c8cd0f9760ecbc5
Mohsen Fattahi <mohsen.fattahi@rashasoft.ir>
2020-10-23 14:54:31 +0330

    recommender-resource and requirements.txt changed .

--- recommender-resource.py
+++ recommender-resource.py
@@ -5,14 +5,14 @@ from model import Model
 import os 
 
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_folder='./ui', static_url_path='/')
 CORS(app)
 
 BASE_PATH = "/movie-recommender"
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    return app.send_static_file('index.html')
 
 @app.route(BASE_PATH + "/recommended-movies", methods=["GET", "POST"])
 def get_recommended_movies():
@@ -31,4 +31,4 @@ def get_recommended_movies():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
