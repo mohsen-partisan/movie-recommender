c051ff64e67efe2809cfdcf48be291c61e631780
Mohsen Fattahi <mohsen.fattahi@rashasoft.ir>
2020-10-27 22:13:49 +0330

    a search button added.

--- recommender-resource.py
+++ recommender-resource.py
@@ -30,4 +30,4 @@ def get_recommended_movies():
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', debug=False, port=os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 5000))
--- ui/app.js
+++ ui/app.js
@@ -1,22 +1,22 @@
-const searchUser = document.getElementById('searchUser');
+var movieName;
+const searchBtn = document.getElementById('btn');
 
 const movieRecommender = new MovieRecommender();
 const ui = new UI();
-searchUser.addEventListener('keyup', e => {
-    var movieName = e.target.value;
-    if (e.keyCode === 13) {
-        if (movieName !== '') {
-            movieName = movieName.replace(/[^0-9A-Z]+/gi, '');
-            movieName = movieName.toLowerCase();
-            movieRecommender.getMovies(movieName).then(data => {
-                var movieInfos = [];
-                for (var i = 0; i <= 9; i++) {
-                    movieInfos.push(data.allMovieInfo[i]);
-                }
-                ui.showUserProfile(movieInfos);
-            });
-        } else {
-            ui.clearProfile();
-        }
+
+searchBtn.addEventListener('click', e => {
+    movieName = document.getElementById('searchUser').value;
+    if (movieName !== '') {
+        movieName = movieName.replace(/[^0-9A-Z]+/gi, '');
+        movieName = movieName.toLowerCase();
+        movieRecommender.getMovies(movieName).then(data => {
+            var movieInfos = [];
+            for (var i = 0; i <= 9; i++) {
+                movieInfos.push(data.allMovieInfo[i]);
+            }
+            ui.showUserProfile(movieInfos);
+        });
+    } else {
+        ui.clearProfile();
     }
 });
